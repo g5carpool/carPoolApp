6b363256fbe671f97a179ecac637f32251f8b1d0
Alinura <alinura.niyazova13@gmail.com>
2019-04-01 15:47:26 +0100

    add create new journey page/features

--- car-pool-app/src/components/App/index.js
+++ car-pool-app/src/components/App/index.js
@@ -8,6 +8,7 @@ import SignInPage from '../SignIn';
 import HomePage from '../Home';
 import AdminPage from '../Admin';
 import AccountPage from '../Account';
+import Driver from '../Driver';
 
 import * as ROUTES from '../../constants/routes';
 import { withAuthentication } from '../Session';
@@ -22,7 +23,7 @@ const App = () => (
       <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route path={ROUTES.REGISTER} component={RegisterPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      
+      <Route path={ROUTES.DRIVER} component={Driver} />
       <Route path={ROUTES.HOME} component={HomePage} />
       <Route path={ROUTES.ACCOUNT} component={AccountPage} />
       <Route path={ROUTES.ADMIN} component={AdminPage} />
---
+++ car-pool-app/src/components/Driver/index.js
@@ -0,0 +1,16 @@
+import React, {Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+import Form from "./user";
+    
+    class Driver extends Component {
+        render() {
+            return (
+                <div className="Driver">
+                <Form/>
+                </div>
+            );
+        }
+    }
+    export default
+    Driver;
